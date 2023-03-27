e0201d6920dea084a9dc4dc4b4328f0e5d29fa42
grandiloquent <1785907865@qq.com>
2023-03-27 19:42:43 +0800

    更新

--- app/src/main/rust/src/lib.rs
+++ app/src/main/rust/src/lib.rs
@@ -1,21 +1,19 @@
-mod java_glue;
+use jni::{
+    objects::JObject,
+    sys::{jboolean, jint, jobject},
+    JNIEnv,
+};
+
 use std::{net::SocketAddr, string, collections::HashMap};
-
-pub use crate::java_glue::*;
-
 // https://docs.rs/android_logger/0.13.1/android_logger/
 use android_logger::Config;
-use jni_sys::{jobject, JNIEnv};
 use log::LevelFilter;
-use rifgen::rifgen_attr::*;
 use std::{ffi::CString, io::Error, ptr::NonNull};
 use ndk::asset::AssetManager;
 use warp::{
     filters::cors::CorsForbidden, http::Method, http::StatusCode, reject::Reject, Filter,
     Rejection, Reply,
 };
-
-pub struct RustLog;
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
     if let Some(error) = r.find::<CorsForbidden>() {
@@ -31,7 +29,7 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
     }
 }
 #[tokio::main]
-async fn run_server(host: &str, asset_manager_object:  jobject) {
+async fn run_server(host: &str) {
     let cors = warp::cors()
         .allow_any_origin()
         .allow_header("content-type")
@@ -47,30 +45,24 @@ async fn run_server(host: &str, asset_manager_object:  jobject) {
     let server: SocketAddr = host.parse().expect("Unable to parse socket address");
     warp::serve(routes).run(server).await;
 }
-impl RustLog {
-    //set up logging
-    #[generate_interface]
-    pub fn start_server(host: &str, asset_manager_object:  jobject) {
+fn start_server(host: &str) {
         #[cfg(target_os = "android")]
         android_logger::init_once(
             Config::default()
                 .with_max_level(LevelFilter::Trace)
                 .with_tag("Rust"),
         );
-        run_server(host,asset_manager_object);
+        run_server(host);
     }
-}
+
 async fn get_home(params: HashMap<String, String>) -> Result<impl warp::Reply, warp::Rejection> {
     return Ok(warp::reply::with_status("Question updated", StatusCode::OK));
 }
-fn get_asset_manager(env: *mut JNIEnv, asset_manager_object:  jobject) -> AssetManager {
-    let aasset_manager_pointer = unsafe {
-        ndk_sys::AAssetManager_fromJava(env, asset_manager_object)
-    };
-    let asset_manager = unsafe {
-        ndk::asset::AssetManager::from_ptr(NonNull::<ndk_sys::AAssetManager>::new_unchecked(
-            aasset_manager_pointer,
-        ))
-    };
-    asset_manager
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "C" fn Java_psycho_euphoria_killer_startServer(
+    env: JNIEnv,
+    _class: jni::objects::JClass,
+    asset_manager: JObject,
+) {
 }
