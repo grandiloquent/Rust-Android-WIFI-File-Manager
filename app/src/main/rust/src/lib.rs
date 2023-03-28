aee21ea140edcdbe794cb524e2fd096c73d9b436
grandiloquent <1785907865@qq.com>
2023-03-28 21:36:42 +0800

    更新

--- app/src/main/rust/src/lib.rs
+++ app/src/main/rust/src/lib.rs
@@ -1,208 +1,28 @@
 #![allow(unused_qualifications)]
 
+mod utils;
+
 use std::collections::HashMap;
-use std::error::Error;
-use std::{fmt, io};
-use std::net::SocketAddr;
 use std::ptr::NonNull;
-use std::sync::Arc;
-use bytes::BytesMut;
-use http::{HeaderValue, Request, Response, StatusCode};
 use jni::JNIEnv;
 use jni::objects::{JObject, JString};
 use ndk::asset::AssetManager;
-use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::RwLock;
-use tokio_stream::StreamExt;
-use tokio_util::codec::{Decoder, Encoder, Framed};
 use serde::{Deserialize, Serialize};
-use futures::sink::SinkExt;
+use tiny_http::{Server, Response, Header, Request};
+use crate::utils::{extension_to_mime, get_asset_manager, get_header, read_asset, read_resource_file};
 
-#[tokio::main]
-async unsafe fn run_server(host: &str, ass: AssetManager) {
-    let server = TcpListener::bind(host).await.unwrap();
-    println!("Listening on: {}", host);
 
-    loop {
-        let (stream, _) = server.accept().await.unwrap();
-        tokio::spawn(async move {
-            if let Err(e) = process(stream).await {
-                println!("failed to process connection; error = {}", e);
-            }
-        });
-    }
-}
-
-async fn process(stream: TcpStream) -> Result<(), Box<dyn Error>> {
-    let mut transport = Framed::new(stream, Http);
-
-    while let Some(request) = transport.next().await {
-        match request {
-            Ok(request) => {
-                let response = respond(request).await?;
-
-                transport.send(response).await?;
-            }
-            Err(e) => return Err(e.into()),
+fn run_server(host: &str, ass: AssetManager) {
+    let server = Server::http(host).unwrap();
+    let cache: HashMap<String, String> = HashMap::new();
+    let headers: HashMap<String, Header> = HashMap::new();
+    for request in server.incoming_requests() {
+        let url = request.url();
+        if url == "/" {
+            let data = read_asset("index.html", cache.clone(), &ass);
+            let _ = request.respond(Response::from_string(data)
+                .with_header(get_header("index.html", &headers)));
         }
-    }
-
-    Ok(())
-}
-
-
-async fn respond(req: Request<()>) -> Result<Response<String>, Box<dyn Error>> {
-    let mut response = Response::builder();
-    let body = match req.uri().path() {
-        "/plaintext" => {
-            response = response.header("Content-Type", "text/plain");
-            "Hello, World!".to_string()
-        }
-        "/json" => {
-            response = response.header("Content-Type", "application/json");
-
-            #[derive(Serialize)]
-            struct Message {
-                message: &'static str,
-            }
-            serde_json::to_string(&Message {
-                message: "Hello, World!",
-            })?
-        }
-        _ => {
-            response = response.status(StatusCode::NOT_FOUND);
-            String::new()
-        }
-    };
-    let response = response
-        .body(body)
-        .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
-
-    Ok(response)
-}
-
-struct Http;
-
-/// Implementation of encoding an HTTP response into a `BytesMut`, basically
-/// just writing out an HTTP/1.1 response.
-impl Encoder<Response<String>> for Http {
-    type Error = io::Error;
-
-    fn encode(&mut self, item: Response<String>, dst: &mut BytesMut) -> io::Result<()> {
-        use std::fmt::Write;
-
-        write!(
-            BytesWrite(dst),
-            "\
-             HTTP/1.1 {}\r\n\
-             Server: Example\r\n\
-             Content-Length: {}\r\n\
-             ",
-            item.status(),
-            item.body().len(),
-        )
-            .unwrap();
-
-        for (k, v) in item.headers() {
-            dst.extend_from_slice(k.as_str().as_bytes());
-            dst.extend_from_slice(b": ");
-            dst.extend_from_slice(v.as_bytes());
-            dst.extend_from_slice(b"\r\n");
-        }
-
-        dst.extend_from_slice(b"\r\n");
-        dst.extend_from_slice(item.body().as_bytes());
-
-        return Ok(());
-
-        // Right now `write!` on `Vec<u8>` goes through io::Write and is not
-        // super speedy, so inline a less-crufty implementation here which
-        // doesn't go through io::Error.
-        struct BytesWrite<'a>(&'a mut BytesMut);
-
-        impl fmt::Write for BytesWrite<'_> {
-            fn write_str(&mut self, s: &str) -> fmt::Result {
-                self.0.extend_from_slice(s.as_bytes());
-                Ok(())
-            }
-
-            fn write_fmt(&mut self, args: fmt::Arguments<'_>) -> fmt::Result {
-                fmt::write(self, args)
-            }
-        }
-    }
-}
-
-impl Decoder for Http {
-    type Item = Request<()>;
-    type Error = io::Error;
-
-    fn decode(&mut self, src: &mut BytesMut) -> io::Result<Option<Request<()>>> {
-        // TODO: we should grow this headers array if parsing fails and asks
-        //       for more headers
-        let mut headers = [None; 16];
-        let (method, path, version, amt) = {
-            let mut parsed_headers = [httparse::EMPTY_HEADER; 16];
-            let mut r = httparse::Request::new(&mut parsed_headers);
-            let status = r.parse(src).map_err(|e| {
-                let msg = format!("failed to parse http request: {:?}", e);
-                io::Error::new(io::ErrorKind::Other, msg)
-            })?;
-
-            let amt = match status {
-                httparse::Status::Complete(amt) => amt,
-                httparse::Status::Partial => return Ok(None),
-            };
-
-            let toslice = |a: &[u8]| {
-                let start = a.as_ptr() as usize - src.as_ptr() as usize;
-                assert!(start < src.len());
-                (start, start + a.len())
-            };
-
-            for (i, header) in r.headers.iter().enumerate() {
-                let k = toslice(header.name.as_bytes());
-                let v = toslice(header.value);
-                headers[i] = Some((k, v));
-            }
-
-            let method = http::Method::try_from(r.method.unwrap())
-                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-
-            (
-                method,
-                toslice(r.path.unwrap().as_bytes()),
-                r.version.unwrap(),
-                amt,
-            )
-        };
-        if version != 1 {
-            return Err(io::Error::new(
-                io::ErrorKind::Other,
-                "only HTTP/1.1 accepted",
-            ));
-        }
-        let data = src.split_to(amt).freeze();
-        let mut ret = Request::builder();
-        ret = ret.method(method);
-        let s = data.slice(path.0..path.1);
-        let s = unsafe { String::from_utf8_unchecked(Vec::from(s.as_ref())) };
-        ret = ret.uri(s);
-        ret = ret.version(http::Version::HTTP_11);
-        for header in headers.iter() {
-            let (k, v) = match *header {
-                Some((ref k, ref v)) => (k, v),
-                None => break,
-            };
-            let value = HeaderValue::from_bytes(data.slice(v.0..v.1).as_ref())
-                .map_err(|_| io::Error::new(io::ErrorKind::Other, "header decode error"))?;
-            ret = ret.header(&data[k.0..k.1], value);
-        }
-
-        let req = ret
-            .body(())
-            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-        Ok(Some(req))
     }
 }
 
@@ -226,16 +46,4 @@ pub extern "C" fn Java_psycho_euphoria_killer_MainActivity_startServer(
     unsafe {
         run_server(_host.as_str(), ass);
     }
-}
-
-pub fn get_asset_manager(env: JNIEnv, asset_manager_object: JObject) -> AssetManager {
-    let aasset_manager_pointer = unsafe {
-        ndk_sys::AAssetManager_fromJava(env.get_native_interface(), *asset_manager_object)
-    };
-    let asset_manager = unsafe {
-        ndk::asset::AssetManager::from_ptr(NonNull::<ndk_sys::AAssetManager>::new_unchecked(
-            aasset_manager_pointer,
-        ))
-    };
-    asset_manager
 }
