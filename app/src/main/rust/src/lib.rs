ea9aeaf7ed08fe4f59a76a25c72d4fea4e4848f2
grandiloquent <1785907865@qq.com>
2023-03-29 02:11:29 +0800

    更新

--- app/src/main/rust/src/lib.rs
+++ app/src/main/rust/src/lib.rs
@@ -4,18 +4,20 @@ mod utils;
 
 use std::collections::HashMap;
 use std::fs::File;
+use std::io::Read;
 use std::ptr::NonNull;
 use jni::JNIEnv;
 use jni::objects::{JObject, JString};
 use ndk::asset::AssetManager;
 use regex::Regex;
 use rusqlite::{Connection, Error};
+use serde_json::Value;
 use tiny_http::{Server, Response, Header, Request, HeaderField};
 use urlencoding::decode;
 
 use crate::utils::{extension_to_mime, get_asset_manager, get_content_disposition, get_file_list, get_header, read_asset, read_resource_file, response_file, StringExt};
 
-fn get_notes(conn:&Connection,limit: &str) -> Result<Vec<HashMap<String, String>>, Error> {
+fn get_notes(conn: &Connection, limit: &str) -> Result<Vec<HashMap<String, String>>, Error> {
     let mut stmt = conn.prepare("select _id,title,update_at from notes ORDER by update_at DESC LIMIT ?1")?;
     let mut rows = stmt.query([limit])?;
     let mut notes: Vec<HashMap<String, String>> = Vec::new();
@@ -43,7 +45,7 @@ fn run_server(host: &str, ass: AssetManager) {
         "CREATE TABLE IF NOT EXISTS notes(_id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT,content TEXT,create_at INTEGER NOT NULL,update_at  INTEGER NOT NULL)",
         (), // empty list of parameters.
     );
-    for request in server.incoming_requests() {
+    for mut request in server.incoming_requests() {
         let original_url = request.url().to_owned();
         let path = original_url.substring_before("?");
         if path == "/" {
@@ -70,10 +72,21 @@ fn run_server(host: &str, ass: AssetManager) {
             let file_path = decode(query.as_str()).unwrap().to_string();
             response_file(file_path, request, files_opened_directly.clone(), headers.clone());
         } else if path == "/api/note" {
-            let list = get_notes(&conn,"50").unwrap();
-            let data = serde_json::to_string(&list).unwrap();
-            let _ = request.respond(Response::from_string(data)
-                .with_header(get_header(".json", &headers)));
+            if request.method().to_string() == "GET" {
+                if original_url.contains("action=") {
+                    let action = original_url.substring_after("action=").substring_before("&");
+                } else {
+                    let list = get_notes(&conn, "50").unwrap();
+                    let data = serde_json::to_string(&list).unwrap();
+                    let _ = request.respond(Response::from_string(data)
+                        .with_header(get_header(".json", &headers)));
+                }
+            } else {
+                let mut content = String::new();
+                request.as_reader().read_to_string(&mut content).unwrap();
+                let json: Value  = serde_json::from_str(content.as_str()).unwrap();
+                log::error!("{}",content);
+            }
         } else if path.starts_with("/api/") {
             let referer = request
                 .headers()
