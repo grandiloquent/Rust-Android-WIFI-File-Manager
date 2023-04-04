8569188e91d135a0a03f85a539c61a4e545d6f8c
grandiloquent <1785907865@qq.com>
2023-04-05 05:30:10 +0800

    更新

--- app/src/main/assets/editor/custom-actions.js
+++ app/src/main/assets/editor/custom-actions.js
@@ -308,123 +308,7 @@
       const wrapper = document.createElement("div");
       wrapper.setAttribute("class", "wrapper");
       const style = document.createElement('style');
-      style.textContent = `.icon {
-  display: inline-block;
-  flex-shrink: 0;
-  width: 24px;
-  height: 24px;
-  fill: currentColor;
-  stroke: none;
-  margin-right: 12px;
-}
-
-button {
-  padding: 0;
-  border: none;
-  outline: none;
-  font: inherit;
-  text-transform: inherit;
-  color: inherit;
-  background: transparent;
-}
-
-button {
-  cursor: pointer;
-  box-sizing: border-box;
-  text-align: initial;
-  text-transform: unset;
-  width: 100%;
-  display: flex;
-  padding: 0;
-  margin-left: 12px;
-  font-size: 1.6rem;
-  line-height: 2.2rem;
-}
-
-.menu-item {
-  padding: 0;
-  height: 48px;
-  display: flex;
-  -webkit-box-align: center;
-  align-items: center;
-  background: #fff;
-  font-size: 1.6rem;
-  line-height: 2.2rem;
-  justify-content: center;
-}
-
-.bottom-sheet-layout-content-wrapper {
-  -webkit-box-flex: 1;
-  flex: 1;
-  overflow-y: scroll;
-  /*max-height: 379.2px;*/
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  background: #dadce0;
-  gap: 1px;
-  border: 1px solid #dadce0;
-}
-
-.bottom-sheet-layout-header-title-wrapper {
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  flex-direction: column;
-  display: flex;
-  margin-left: 12px;
-}
-
-.bottom-sheet-layout-header {
-  -webkit-box-pack: justify;
-  justify-content: space-between;
-  display: flex;
-  margin-top: 8px;
-}
-
-.bottom-sheet-drag-line {
-  background: #0f0f0f;
-  opacity: .15;
-  border-radius: 4px;
-  height: 4px;
-  margin: 0 auto;
-  width: 40px;
-  margin-top: 8px;
-}
-
-.bottom-sheet-layout-header-wrapper {
-  overflow: hidden;
-  -webkit-box-flex: 0;
-  flex: none;
-  border-bottom: 1px solid #fff;
-}
-
-.bottom-sheet-layout {
-  border-radius: 12px;
-  background-color: #fff;
-  display: block;
-  overflow: hidden;
-  position: fixed;
-  margin: 0 8px 24px;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: 2;
-}
-
-.overlay {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: 1;
-  cursor: pointer;
-  background-color: rgba(0, 0, 0, .6);
-}
-
-.wrapper {
-  position: fixed;
-  z-index: 5;
-}`;
+      style.textContent = `.icon{display:inline-block;flex-shrink:0;width:24px;height:24px;fill:currentColor;stroke:none;margin-right:12px}button{padding:0;border:none;outline:none;font:inherit;text-transform:inherit;color:inherit;background:transparent}button{cursor:pointer;box-sizing:border-box;text-align:initial;text-transform:unset;width:100%;display:flex;padding:0;margin-left:12px;font-size:1.6rem;line-height:2.2rem}.menu-item{padding:0;height:48px;display:flex;-webkit-box-align:center;align-items:center;background:#fff;font-size:1.6rem;line-height:2.2rem;justify-content:center}.bottom-sheet-layout-content-wrapper{-webkit-box-flex:1;flex:1;overflow-y:scroll;display:grid;grid-template-columns:repeat(3,1fr);background:#dadce0;gap:1px;border:1px solid #dadce0}.bottom-sheet-layout-header-title-wrapper{-webkit-box-orient:vertical;-webkit-box-direction:normal;flex-direction:column;display:flex;margin-left:12px}.bottom-sheet-layout-header{-webkit-box-pack:justify;justify-content:space-between;display:flex;margin-top:8px}.bottom-sheet-drag-line{background:#0f0f0f;opacity:.15;border-radius:4px;height:4px;margin:0 auto;width:40px;margin-top:8px}.bottom-sheet-layout-header-wrapper{overflow:hidden;-webkit-box-flex:0;flex:none;border-bottom:1px solid #fff}.bottom-sheet-layout{border-radius:12px;background-color:#fff;display:block;overflow:hidden;position:fixed;margin:0 8px 24px;bottom:0;left:0;right:0;z-index:2}.overlay{position:fixed;top:0;bottom:0;left:0;right:0;z-index:1;cursor:pointer;background-color:rgba(0,0,0,.6)}.wrapper{position:fixed;z-index:5}`;
       this.wrapper = wrapper;
       this.shadowRoot.append(style, wrapper);
     }
@@ -441,46 +325,49 @@ button {
     }
     connectedCallback() {
       this.wrapper.innerHTML = `<div bind @click="close" class="overlay">
-</div>
-<div class="bottom-sheet-layout">
-  <div class="bottom-sheet-layout-header-wrapper">
-    <div class="bottom-sheet-drag-line">
     </div>
-    <div class="bottom-sheet-layout-header">
-      <div class="bottom-sheet-layout-header-title-wrapper">
+    <div class="bottom-sheet-layout">
+      <div class="bottom-sheet-layout-header-wrapper">
+        <div class="bottom-sheet-drag-line">
+        </div>
+        <div class="bottom-sheet-layout-header">
+          <div class="bottom-sheet-layout-header-title-wrapper">
+          </div>
+          <div bind="contentWrapper" class="bottom-sheet-layout-content-wrapper">
+            <div bind @click="insertLink" class="menu-item">
+              插入链接
+            </div>
+            <div bind @click="openLink" class="menu-item">
+              打开链接
+            </div>
+            <div bind @click="onPreview" class="menu-item">
+              预览
+            </div>
+            <div bind @click="onTranslateChinese" class="menu-item">
+              翻译中文
+            </div>
+            <div bind @click="onTranslateEnglish" class="menu-item">
+              翻译英文
+            </div>
+            <div bind @click="sortLines" class="menu-item">
+              排序
+            </div>
+            <div bind @click="uploadHanlder" class="menu-item">
+              上传图片
+            </div>
+            <div bind @click="onEval" class="menu-item">
+              执行表达式
+            </div>
+            <div bind @click="onDeleteString" class="menu-item">
+              删除
+            </div>
+            <div bind @click="close" class="menu-item">
+              取消
+            </div>
+          </div>
+        </div>
       </div>
-      <div bind="contentWrapper" class="bottom-sheet-layout-content-wrapper">
-        <div bind @click="insertLink" class="menu-item">
-          插入链接
-        </div>
-        <div bind @click="openLink" class="menu-item">
-          打开链接
-        </div>
-        <div bind @click="onPreview" class="menu-item">
-          预览
-        </div>
-        <div bind @click="onTranslateChinese" class="menu-item">
-          翻译中文
-        </div>
-        <div bind @click="onTranslateEnglish" class="menu-item">
-          翻译英文
-        </div>
-        <div bind @click="sortLines" class="menu-item">
-          排序
-        </div>
-        <div bind @click="uploadHanlder" class="menu-item">
-          上传图片
-        </div>
-        <div bind @click="onEval" class="menu-item">
-          执行表达式
-        </div>
-        <div bind @click="close" class="menu-item">
-          取消
-        </div>
-      </div>
-    </div>
-  </div>
-</div>`;
+    </div>`;
       this.wrapper.querySelectorAll('[bind]').forEach(element => {
         if (element.getAttribute('bind')) {
           this[element.getAttribute('bind')] = element;
@@ -589,6 +476,15 @@ button {
         });
         input.click();
       });
+    }
+    onDeleteString() {
+      const start = textarea.selectionStart;
+      let end = textarea.selectionEnd;
+      while (end + 1 < textarea.value.length && textarea.value[end] !== ']') {
+        end++;
+      }
+      textarea.setRangeText('', start, end, 'end');
+      console.log(start);
     }
 
   }
